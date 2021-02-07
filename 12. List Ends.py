56db8fad35ac84ed29d96d0f96195dcbc90e3283
TechTrantor <34548416+TrantorT@users.noreply.github.com>
2021-02-07 20:32:06 +0100

    update

---
+++ 12. List Ends.py
@@ -0,0 +1,8 @@
+# Write a program that takes a list of numbers (for example, a = [5, 10, 15, 20, 25]) 
+# and makes a new list of only the first and last elements of the given list. For practice, write this code inside a function.
+
+
+for i in range(1-10):
+    list = [i]
+
+print(list)
