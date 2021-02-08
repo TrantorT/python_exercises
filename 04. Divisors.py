0d737073e0a422f66a014f0ac7552dc0668b5524
TechTrantor <34548416+TrantorT@users.noreply.github.com>
2021-02-08 11:46:00 +0100

    Update 04. Divisors.py

--- 04. Divisors.py
+++ 04. Divisors.py
@@ -1,11 +1,11 @@
 #https://www.practicepython.org/exercise/2014/02/26/04-divisors.html
 
-numero = int(input("Qual o numero ? "))
+numero = int(input("Pick a number! "))
 
 i=1
 while i < numero:
     if numero % i == 0 :
-        print(f"{i} 'e divisor de {numero}")
+        print(f"{i} is a Divisor of {numero}")
         i+=1
     else:
         i+=1
