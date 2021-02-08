7f393278a9cbc32b06ddb8b94a0576da601f41ba
TechTrantor <34548416+TrantorT@users.noreply.github.com>
2021-02-08 16:14:23 +0100

    Create 14. List Remove Duplicates.py

---
+++ 14. List Remove Duplicates.py
@@ -0,0 +1,15 @@
+import random
+
+lista1 = []
+for i in range(0,50):
+    n = random.randint(1,30)
+    lista1.append(n)
+print(lista1)
+
+#a set has no duplicates, thats the easy way
+x = set(lista2)
+print(x)
+
+#another list
+lista2 = []
+#for i in lista1:
