9ac0bbec4395eb3bfb22e6b478aafd49f0668793
TechTrantor <34548416+TrantorT@users.noreply.github.com>
2021-02-08 10:54:58 +0100

    Update 01 Character Input.py

--- 01 Character Input.py
+++ 01 Character Input.py
@@ -1,10 +1,13 @@
 #https://www.practicepython.org/exercise/2014/01/29/01-character-input.html
 
-name = input("Hello, what is your name? ")
-age = input("and your age?")
+name = input("Hello, what is your name?\n")
+age = input("and your age?\n")
 
 idade = int(age)
 
 x = (100 - idade) + 2021
 
-print(f"Hello {name}, you will turn 100 years old in {x}")
+if idade > 10 and idade < 90:
+    print(f"Hello {name}, you will turn 100 years old in {x}")
+else:
+    print(f"Hello {name}, I don't really believe your age, but according to my calculations you would turn 100 years old in {x}")
