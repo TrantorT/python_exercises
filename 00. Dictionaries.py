3f2412f9364a0fbb2c736b8ffe1ffd3f0b0ee0f2
TechTrantor <34548416+TrantorT@users.noreply.github.com>
2021-02-08 11:37:22 +0100

    Create 00. Dictionaries.py

---
+++ 00. Dictionaries.py
@@ -0,0 +1,19 @@
+#construct 1
+d ={'key':'value',
+'key1':"value1"}
+
+print(d)
+
+#construct 2
+d1 =dict({'key':'value',
+'key1':"value1"})
+
+print(d1)
+
+#access elements
+
+print(d1['key'])
+
+#adding a new entry
+d1['key3']='value3'
+print(d1)
