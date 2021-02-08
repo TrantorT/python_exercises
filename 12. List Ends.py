6e14b1037ba0a1888c642e1adc521cc18af46b1e
TechTrantor <34548416+TrantorT@users.noreply.github.com>
2021-02-08 11:53:10 +0100

    Update 12. List Ends.py

--- 12. List Ends.py
+++ 12. List Ends.py
@@ -1,8 +1,16 @@
 # Write a program that takes a list of numbers (for example, a = [5, 10, 15, 20, 25]) 
 # and makes a new list of only the first and last elements of the given list. For practice, write this code inside a function.
 
+import random
 
-for i in range(1-10):
-    list = [i]
+size = int(input("What size of list you want?:\n"))
+lista1 = []
+for i in range(0,size):
+    n = random.randint(1,30)
+    lista1.append(n)
+print(f"This is the original List with {size} elements:\n {lista1}")
 
-print(list)
+newlist = []
+newlist.append(lista1[0])
+newlist.append(lista1[-1])
+print(f"This is the new list with 1st and last element:\n {newlist}")
