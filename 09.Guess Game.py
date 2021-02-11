6e3c184e8fc07488a62f6730d14e5d0dc1eb2bfd
TechTrantor <34548416+TrantorT@users.noreply.github.com>
2021-02-11 10:30:56 +0100

    Update 09.Guess Game.py

--- 09.Guess Game.py
+++ 09.Guess Game.py
@@ -2,19 +2,19 @@
 
 import random
 
-numero = random.randint(1,9)
+numero = random.randint(1,10)
 bingo = False
 count = 0
 
 while bingo == False:
     count+=1
-    escolha = int(input("Adivinha um numero entre 1-9: "))
+    escolha = int(input("Guess a number between 1-10: "))
     if (escolha>numero):
-        print("Numero demasiado alto")
+        print("Number too high!")
     elif (escolha<numero):
-        print("Numero demasiado baixo")
+        print("Number too low!")
     else:
         print("BINGO!!!")
         bingo = True
 
-print(f"Acertaste em {count} tentativas!")
+print(f"You got it in {count} attempts!")
