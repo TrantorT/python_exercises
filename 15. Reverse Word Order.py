9e2b519ef18aac8766205b23c0854af8939d791e
TechTrantor <34548416+TrantorT@users.noreply.github.com>
2021-02-16 09:22:12 +0100

    Create 15. Reverse Word Order.py

---
+++ 15. Reverse Word Order.py
@@ -0,0 +1,15 @@
+mystring = "My name is Daniel"
+print(f"The initial string has length of: {len(mystring)}")
+
+revstr =""
+
+for i in mystring.split():
+    print(i)
+      
+
+
+print(len(mystring.split()))
+
+
+
+#print(mystring[::-1])
