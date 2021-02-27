d76b20935258948167f96ef309c2b8443afc41bd
TechTrantor <34548416+TrantorT@users.noreply.github.com>
2021-02-27 20:29:24 +0100

    Update 17. Decode A Web Page.py

--- 17. Decode A Web Page.py
+++ 17. Decode A Web Page.py
@@ -1,19 +1,20 @@
 import bs4
 import requests
 
-url = 'http://www.record.pt'
+url = 'http://www.publico.pt'
 r = requests.get(url)
 r_html = r.text
 
 soup = bs4.BeautifulSoup(r_html,'html.parser')
 
+
 #print(soup)
 
 title = soup.find('title').string
 
-titulos=soup.find_all(class_="eventAnalytics")
+titulos=soup.find_all(class_="card__title headline")
 for t in titulos:
-    print(t.text)
+    print(t.find_all(class_="card__faux-block-link"))
     
 
 #print(titulos)
